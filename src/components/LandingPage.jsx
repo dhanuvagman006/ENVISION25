beb3232293fbed3b9f2dbb6cdf6e3c5ce3641b1c
dhanuvagman006 <dhnaushinsit@gmail.com>
2025-04-13 08:53:01 +0530

    minor fixes

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,8 @@
+function LandingPage(){
+    return (
+        <>
+        <h1>Hello im ur Landing Page</h1>
+        </>
+    )
+}
+export default LandingPage;
---
+++ src/components/Loginpage.jsx
@@ -0,0 +1,8 @@
+function LoginPage(){
+    return(
+        <>
+        <h1>Hello im ur Login Page</h1>
+        </>
+    )
+}
+export default LoginPage;
