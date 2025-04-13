63addba0e51cc64a850b6bd15a6e74453f6ee234
dhanuvagman006 <dhnaushinsit@gmail.com>
2025-04-13 12:54:55 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,30 @@
-function LandingPage(){
-    return (
-        <>
-        <h1>Hello im ur Landing Page</h1>
-        </>
-    )
+import React from "react";
+import mainBg from "../assets/homebg.png"; // Ensure path is correct
+import NavBar from "./Navbar";
+function LandingPage() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${mainBg})`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          padding: "2rem",
+          minHeight: "200vh",
+          color: "white", 
+          backgroundColor: "rgba(0, 0, 0, 0.5)", 
+        }}
+      >
+        <NavBar/>
+      </div>
+    </div>
+  );
 }
+
 export default LandingPage;
